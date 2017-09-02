1e477bd60d9153ecd88a9901e22e98940dc260d2
Ashaba <jnashaba4@gmail.com>
2017-09-02 12:29:30 +0300

    test(Check): test successful Check creation and badge
    - test for badge request, status codes for bad and successful requests depict right urls passed
    
    [Delivers #150627412]

--- hc/api/tests/test_badge.py
+++ hc/api/tests/test_badge.py
@@ -12,13 +12,17 @@ class BadgeTestCase(BaseTestCase):
         self.check = Check.objects.create(user=self.alice, tags="foo bar")
 
     def test_it_rejects_bad_signature(self):
+        """ Assert for status code 400 """
+        
         r = self.client.get("/badge/%s/12345678/foo.svg" % self.alice.username)
-        ### Assert the expected response status code
+        self.assertEqual(r.status_code, 400)
 
     def test_it_returns_svg(self):
+        """Assert that the svg is returned if the response status code is 200"""
+        
         sig = base64_hmac(str(self.alice.username), "foo", settings.SECRET_KEY)
         sig = sig[:8].decode("utf-8")
         url = "/badge/%s/%s/foo.svg" % (self.alice.username, sig)
 
         r = self.client.get(url)
-        ### Assert that the svg is returned
+        self.assertEqual(r.status_code, 200)
