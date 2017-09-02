d31152b578e4320c0c3e9b9baf2f01481b25df4e
Ashaba <jnashaba4@gmail.com>
2017-09-02 12:29:30 +0300

    test(Check): test successful Check creation and badge
    - test for badge request, status codes for bad and successful requests depict right urls passed
    
    [Delivers #150627412]

--- hc/api/tests/test_admin.py
+++ hc/api/tests/test_admin.py
@@ -10,12 +10,16 @@ class ApiAdminTestCase(BaseTestCase):
         self.check = Check.objects.create(user=self.alice, tags="foo bar")
         
     def test_set_alice_as_superuser_and_staff(self):
+        """Update user permission to superuser and staff"""
+        
         User.objects.filter(username=self.alice).update(is_superuser=True, is_staff=True)
         self.alice = User.objects.get(id=self.alice.id)
         self.assertEqual(self.alice.is_superuser, True)
         self.assertTrue(self.alice.is_staff)
 
     def test_it_shows_channel_list_with_pushbullet(self):
+        """Test for successful creation of Channel, with kind field"""
+        
         self.client.login(username="alice@example.org", password="password")
 
         ch = Channel(user=self.alice, kind="pushbullet", value="test-token")
