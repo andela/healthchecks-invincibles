7d3ad35343bb2e6e1bce723c58d32b894cf50dda
Ashaba <jnashaba4@gmail.com>
2017-09-02 12:55:00 +0300

    test(Check): test check creation not in grace period
    - when a new check is created, esnure it is not in grace period
    
    [Delivers #150627412]

--- hc/api/tests/test_check_model.py
+++ hc/api/tests/test_check_model.py
@@ -18,17 +18,16 @@ class CheckModelTestCase(TestCase):
         self.assertEqual(len(check.tags_list()), 0)
 
     def test_status_works_with_grace_period(self):
+        """test for check status and the grace period"""
         check = Check()
 
         check.status = "up"
         check.last_ping = timezone.now() - timedelta(days=1, minutes=30)
-
         self.assertTrue(check.in_grace_period())
         self.assertEqual(check.get_status(), "up")
 
-        ### The above 2 asserts fail. Make them pass
-
     def test_paused_check_is_not_in_grace_period(self):
+        """  Test that when a new check is created, it is not in the grace period"""
         check = Check()
 
         check.status = "up"
@@ -38,4 +37,3 @@ class CheckModelTestCase(TestCase):
         check.status = "paused"
         self.assertFalse(check.in_grace_period())
 
-    ### Test that when a new check is created, it is not in the grace period
