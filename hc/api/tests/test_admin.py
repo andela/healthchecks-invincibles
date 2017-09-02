45b6fea1473fb47cc0c3ee7681e672fa65861d51
Ashaba <jnashaba4@gmail.com>
2017-09-02 11:37:07 +0300

    test(userModel): test to update user
    - test to update the user permissions of an existing user as staff
    - test to update the user permissions of an existing user as a superuser
    
    [Delivers #150627412]

--- hc/api/tests/test_admin.py
+++ hc/api/tests/test_admin.py
@@ -1,5 +1,6 @@
 from hc.api.models import Channel, Check
 from hc.test import BaseTestCase
+from django.contrib.auth.models import User
 
 
 class ApiAdminTestCase(BaseTestCase):
@@ -7,8 +8,12 @@ class ApiAdminTestCase(BaseTestCase):
     def setUp(self):
         super(ApiAdminTestCase, self).setUp()
         self.check = Check.objects.create(user=self.alice, tags="foo bar")
-
-        ### Set Alice to be staff and superuser and save her :)
+        
+    def test_set_alice_as_superuser_and_staff(self):
+        User.objects.filter(username=self.alice).update(is_superuser=True, is_staff=True)
+        self.alice = User.objects.get(id=self.alice.id)
+        self.assertEqual(self.alice.is_superuser, True)
+        self.assertTrue(self.alice.is_staff)
 
     def test_it_shows_channel_list_with_pushbullet(self):
         self.client.login(username="alice@example.org", password="password")
