53254c8cf08e8064986ea386a98021d4b9133078
Lornah Lynn Tumuhairwe <lornah.tumuhairwe@andela.com>
2017-09-05 11:28:09 +0300

    bg(tests): Tests for switch teams - tests for checking team membership - tests for wsitching to own team

--- hc/accounts/tests/test_switch_team.py
+++ hc/accounts/tests/test_switch_team.py
@@ -14,6 +14,8 @@ class SwitchTeamTestCase(BaseTestCase):
         r = self.client.get(url, follow=True)
 
         ### Assert the contents of r
+        print (r.context['checks'][0].__dict__)
+        self.assertEqual(r.context['checks'][0].name, "This belongs to Alice")
 
 
     def test_it_checks_team_membership(self):
@@ -22,6 +24,7 @@ class SwitchTeamTestCase(BaseTestCase):
         url = "/accounts/switch_team/%s/" % self.alice.username
         r = self.client.get(url)
         ### Assert the expected error code
+        self.assertEqual(r.status_code, 403)
 
     def test_it_switches_to_own_team(self):
         self.client.login(username="alice@example.org", password="password")
@@ -29,3 +32,4 @@ class SwitchTeamTestCase(BaseTestCase):
         url = "/accounts/switch_team/%s/" % self.alice.username
         r = self.client.get(url, follow=True)
         ### Assert the expected error code
+        self.assertEqual(r.status_code, 200)
