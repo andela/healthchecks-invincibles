3753cd0d524f615d5e508b4e79461d2be68d0436
Annettesunday <Anetsunday@gmail.com>
2017-09-05 10:12:34 +0300

    bg(tests):Add tests to test_pause.py

--- hc/api/tests/test_pause.py
+++ hc/api/tests/test_pause.py
@@ -13,6 +13,8 @@ class PauseTestCase(BaseTestCase):
                              HTTP_X_API_KEY="abc")
 
         ### Assert the expected status code and check's status
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(check.status, "paused")
 
     def test_it_validates_ownership(self):
         check = Check(user=self.bob, status="up")
@@ -24,4 +26,8 @@ class PauseTestCase(BaseTestCase):
 
         self.assertEqual(r.status_code, 400)
 
-        ### Test that it only allows post requests
+    def test_allows_only_post_request(self):
+        """Test that it only allows post requests"""
+        url = "/api/v1/checks/1659718b-21ad-4ed1-8740-43afc6c41524/pause"
+        r = self.client.get(url, HTTP_X_API_KEY="abc")
+        self.assertEqual(r.status_code, 405)
