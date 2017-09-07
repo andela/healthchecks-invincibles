14119a29b789a55a04b970e8b8c58738ecc23490
Annettesunday <Anetsunday@gmail.com>
2017-09-07 12:29:49 +0300

    test(sendalerttests): add more tests for send alerts - Assert when Command's handle many that when handle_many should return True

--- hc/api/tests/test_ping.py
+++ hc/api/tests/test_ping.py
@@ -94,7 +94,7 @@ class PingTestCase(TestCase):
         current_status = self.check.status
         self.assertNotEqual(initial_status, current_status)
 
-    ### Test that the csrf_client head works
+    # Test that the csrf_client head works
     def test_csrf_client_works(self):
        client = Client(enforce_csrf_checks=True)
        response = client.get("/ping/%s/" % self.check.code)
