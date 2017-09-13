978b2751dccbd97d6d7e1ceab0c31c3cccc0b2ef
Lornah Lynn Tumuhairwe <lornah.tumuhairwe@andela.com>
2017-09-13 19:05:18 +0300

    ch(heroku deployment): Set COMPRESS_ENABLED to False

--- hc/settings.py
+++ hc/settings.py
@@ -146,7 +146,7 @@ STATICFILES_FINDERS = (
     'compressor.finders.CompressorFinder',
 )
 COMPRESS_OFFLINE = True
-COMPRESS_ENABLED = True
+COMPRESS_ENABLED = False
 
 # EMAIL_BACKEND = "djmail.backends.default.EmailBackend"
 EMAIL_BACKEND = "sgbackend.SendGridBackend"
