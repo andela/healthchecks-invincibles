d9254b218c68f136f23b0d89fcf91b7da68b0ee7
Lornah Lynn Tumuhairwe <lornah.tumuhairwe@andela.com>
2017-09-13 16:22:27 +0300

    ch(heroku deploy): make SITE ROOT an environment variable

--- hc/settings.py
+++ hc/settings.py
@@ -130,7 +130,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-SITE_ROOT = "http://localhost:8000"
+SITE_ROOT = os.getenv('SITE_ROOT')
 PING_ENDPOINT = SITE_ROOT + "/ping/"
 PING_EMAIL_DOMAIN = HOST
 STATIC_URL = '/static/'
