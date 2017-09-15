b83dd4b03036d4d17063cb1394196f66b9385e93
Lornah Lynn Tumuhairwe <lornah.tumuhairwe@andela.com>
2017-09-15 10:40:57 +0300

    ft(sms integration): Add SMS as alert integration

--- hc/front/views.py
+++ hc/front/views.py
@@ -381,6 +381,11 @@ def add_email(request):
     ctx = {"page": "channels"}
     return render(request, "integrations/add_email.html", ctx)
 
+@login_required
+def add_sms(request):
+    ctx = {"page": "channels"}
+    return render(request, "integrations/add_sms.html", ctx)
+
 
 @login_required
 def add_webhook(request):
