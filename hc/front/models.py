9cbe8688976e063e037b324c5469f4ac911db0c2
Annettesunday <Anetsunday@gmail.com>
2017-09-21 10:10:04 +0300

    feat(models.py): add models for adding a blog

--- hc/front/models.py
+++ hc/front/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+class Blogs(models.Model):
+    blog_post = models.CharField(max_length=20000, blank=False)
+    user = models.TextField()
+    title = models.CharField(max_length=200, blank=False)
+
 # Create your models here.
